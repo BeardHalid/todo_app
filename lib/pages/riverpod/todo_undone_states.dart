a23a107239f0ab57ec7f29fbfd6c8bd389d08702
Abdullah Halid USLU <a.halid.uslu@gmail.com>
2024-02-17 23:48:22 +0300

    State class created for undone tasks

---
+++ lib/pages/riverpod/todo_undone_states.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/models/todo.dart';
+import 'package:todo_app/repository/TodoUndone.dart';
+
+class UndoneTodoProvider extends AsyncNotifier<List<Todo>> {
+  final b = TodoUndone();
+
+  @override
+  FutureOr<List<Todo>> build() async {
+    return b.getTodos("false");
+  }
+
+  Future<void> addTodo(String text) async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async {
+        await b.setTodo(text);
+        return b.getTodos("false");
+      },
+    );
+  }
+
+  Future<void> updateTodo(String id, String checked) async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async {
+        await b.updateTodo(id, checked);
+        return b.getTodos("false");
+      },
+    );
+  }
+
+  Future<void> deleteTodo(String id) async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async {
+        await b.deleteTodo(id);
+        return b.getTodos("false");
+      },
+    );
+  }
+}
+
+final undoneTodoNotifierProvider =
+    AsyncNotifierProvider<UndoneTodoProvider, List<Todo>>(() {
+  return UndoneTodoProvider();
+});
