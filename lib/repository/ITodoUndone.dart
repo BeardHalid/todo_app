8cb1662b0cf0fbe9f0aacf47f7ba136387220910
Abdullah Halid USLU <a.halid.uslu@gmail.com>
2024-02-19 00:10:30 +0300

    Id variable type changed

--- lib/repository/ITodoUndone.dart
+++ lib/repository/ITodoUndone.dart
@@ -2,6 +2,6 @@ import 'package:todo_app/repository/IBase.dart';
 
 abstract class ITodoUndone implements IBase{
   Future<void> setTodo(String text);
-  Future<void> updateTodo(String id, String checked);
-  Future<void> deleteTodo(String id);
+  Future<void> updateTodo(int id, String checked);
+  Future<void> deleteTodo(int id);
 }
