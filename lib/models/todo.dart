2b00d8265f52e18ef821bfe8282fa6f47082eb5c
Abdullah Halid USLU <a.halid.uslu@gmail.com>
2024-02-19 00:11:29 +0300

    Fixed variable

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,11 +1,11 @@
 class Todo{
-  final String id;
+  final int id;
   final String text;
   String checked;
 
   Todo({required this.id, required this.text, required this.checked});
 
   factory Todo.fromJson(Map<String, dynamic> json){
-    return Todo(id: json['id'] as String, text: json['text'] as String, checked: json['checked'] as String);
+    return Todo(id: json['id'] as int, text: json['text'] as String, checked: json['checked'] as String);
   }
 }
