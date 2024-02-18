722ab08cab6670a15dee3c6382a244c17d2871b4
Abdullah Halid USLU <a.halid.uslu@gmail.com>
2024-02-19 00:08:59 +0300

    Edited UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,11 +17,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Anasayfa"),
-      ),
       body: pages[index],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
           onTap: (value) {
             setState(() {
               index = value;
